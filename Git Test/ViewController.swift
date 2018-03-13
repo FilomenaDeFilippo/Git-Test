da05008c3f2792c8013166da3969727c3bcdb789
Mena De Filippo <menadefilippo@cam-03.local>
2018-03-13 11:53:01 +0100

    Creato layout e contatore

--- Git Test/ViewController.swift
+++ Git Test/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var contatore=0
+    
+    @IBAction func button(_ sender: UIButton) {
+        contatore=contatore+1
+        label.text="hai premuto  \(contatore)  volte"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
